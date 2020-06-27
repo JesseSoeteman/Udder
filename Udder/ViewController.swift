cfe854848a8ee4ada59b216c5a0d5f2d3040cc8a
Jesse Soeteman <jessesoeteman@gmail.com>
2020-06-27 21:07:48 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jesse Soeteman on 27/06/2020.
+//  Copyright © 2020 Jesse Soeteman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
